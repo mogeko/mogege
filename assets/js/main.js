5cef13e0813419068bf8c701e58c13c057d512ec
Mogeko <zhengjunyi@live.com>
2020-04-01 16:35:21 +0200

    Update(Feature): Add Content Progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,17 @@ function switchDarkMode() {
         document.body.classList.add('dark-theme');
     }
 })()
+
+// 顶部阅读进度条
+document.addEventListener('DOMContentLoaded', function () {
+    var winHeight = window.innerHeight,
+        docHeight = document.documentElement.scrollHeight,
+        progressBar = document.querySelector('#content_progress');
+    progressBar.max = docHeight - winHeight;
+    progressBar.value = window.scrollY;
+
+    document.addEventListener('scroll', function () {
+          progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
+          progressBar.value = window.scrollY;
+    });
+});
