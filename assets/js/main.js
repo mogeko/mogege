1dc9fb56ad574844802b7f2189d93e5fee64402d
Mogeko <zhengjunyi@live.com>
2020-04-04 01:39:30 +0200

    Fix(Bug): Adapt theme switch button for mobile This button is used to switch between light mode (default) and dark mode Refactored all JavaScript code issue #11

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,51 @@
+
+var _Blog = _Blog || {};
+
 // Dark Mode
-function switchDarkMode() {
-    var night = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
-    if (night == '0'){
-        document.body.classList.add('dark-theme');
-        document.cookie = "dark=1;path=/";
-        console.log('Dark mode on');
-    }else{
+_Blog.switchDarkMode = function() {
+    var currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
+    var isDark = currentTheme === '1';
+    if (currentTheme == '0') {
         document.body.classList.remove('dark-theme');
-        document.cookie = "dark=0;path=/";
-        console.log('Dark mode off');
+    } else if(currentTheme == '1') {
+        document.body.classList.add('dark-theme');
     }
+    // 手动切换 Dark Mode
+    var themeSwitch = document.querySelectorAll('.theme-switch') || isDark;
+    themeSwitch.forEach(function(themeSwitchItem) {
+        themeSwitchItem.addEventListener('click', () => {
+            var currentTheme = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
+            if (currentTheme == '0') {
+                document.body.classList.add('dark-theme');
+                document.cookie = "dark=1;path=/";
+                console.log('Dark mode on');
+            } else {
+                document.body.classList.remove('dark-theme');
+                document.cookie = "dark=0;path=/";
+                console.log('Dark mode off');
+            }
+        });
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var dark = document.cookie.replace(/(?:(?:^|.*;\s*)dark\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
-    if(dark == '0'){
-        document.body.classList.remove('dark-theme');
-    }else if(dark == '1'){
-        document.body.classList.add('dark-theme');
-    }
-});
-
 // 顶部阅读进度条
-document.addEventListener('DOMContentLoaded', function () {
+_Blog.scrollIndicator = function () {
     var winHeight = window.innerHeight,
         docHeight = document.documentElement.scrollHeight,
-        progressBar = document.querySelector('#content_progress'),
-        progressBarMobile = document.querySelector('#content_progress-mobile');
-    progressBar.max, progressBarMobile.max = docHeight - winHeight;
-    progressBar.value, progressBarMobile.value = window.scrollY;
+        progressBar = document.querySelectorAll('.content_progress');
+    progressBar.forEach(function(progressBarItem) {
+        progressBarItem.max = docHeight - winHeight;
+        progressBarItem.value = window.scrollY;
+    });
 
     document.addEventListener('scroll', function () {
-        progressBar.max, progressBarMobile.max = document.documentElement.scrollHeight - window.innerHeight;
-        progressBar.value, progressBarMobile.value = window.scrollY;
+        progressBar.forEach(function(progressBarItem) {
+            progressBarItem.value = window.scrollY;
+        });
     });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    _Blog.switchDarkMode();
+    _Blog.scrollIndicator();
 });
